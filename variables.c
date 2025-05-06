c4c5a8866e0d9ce90b6b19d63365de799ef5edc9
Stanislav Humeniuk <stanislav.humeniuk@outlook.com>
2025-05-06 21:46:36 +0300

    variables training

---
+++ variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int year = 2025;
+  int age = 20;
+
+  printf("It is %d currently, and you are %d years old\n", year, age);
+
+  char grade = 'B';
+  printf("Ypur grade is %c\n", grade);
+
+  char name[] = "Sonorma";
+  printf("Hello, %s\n", name);
+
+  return EXIT_SUCCESS;
+}
